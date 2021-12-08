fe429e07a63ea8f8098ebf4df37aa6dd66de34ea
buli-dot <72310468+buli-dot@users.noreply.github.com>
2021-12-08 23:48:20 +0100

    Add project files.

---
+++ Number Guessing Game1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Number_Guessing_Game1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            Console.WriteLine("Please enter a number between 0 and 100: ");
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            int randNum = r.Next(0,100);
+           while(userInput != randNum)
+            {
+                if(userInput > randNum)
+                {
+                    Console.WriteLine("Too high!");
+                }else if(userInput < randNum)
+                {
+                    Console.WriteLine("Too low!");
+                }
+
+                userInput = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Correct!"); 
+
+        }
+    }
+}
